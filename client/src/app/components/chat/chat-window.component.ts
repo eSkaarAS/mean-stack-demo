a1ec590dc4a9f5a220274026b4099dbb971f8345
eSkaar <erik.skaar@orgbrain.no>
2025-02-19 16:49:52 +0100

    Add radio button selection to chat window component and update styles

--- client/src/app/components/chat/chat-window.component.ts
+++ client/src/app/components/chat/chat-window.component.ts
@@ -6,6 +6,7 @@ import {
   signal,
   WritableSignal,
 } from '@angular/core';
+import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'ob-chat-window',
@@ -19,22 +20,31 @@ import {
         background-color: #f5f5f5;
         gap: 1rem;
       }
+
+      .testerInnerClass {
+        background-color: #f5f5f5;
+      }
     `,
   ],
+  imports: [MatRadioModule],
   template: `
     <section
       class=" bg-primary w-full h-full max-w-5xl mx-auto flex flex-col justify-center items-center"
     >
-      <div class="bg-primary w-full h-full"></div>
+      <div class=" bg-primary w-full h-full testerInnerClass">
+        <mat-radio-group aria-label="Select an option">
+          <mat-radio-button value="1">Option 1</mat-radio-button>
+          <mat-radio-button value="2">Option 2</mat-radio-button>
+        </mat-radio-group>
+      </div>
       <div class="bg-secondary w-full h-20"></div>
     </section>
   `,
 })
 export class ChatWindow implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   count: WritableSignal<number> = signal(0);
   doubleCount: Signal<number> = computed(() => this.count() * 2);
-
-  constructor() {}
-
-  ngOnInit() {}
 }
