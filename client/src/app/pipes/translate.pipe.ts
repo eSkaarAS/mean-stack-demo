c1750b2817a3bb75df3665264dd5134f80ff5378
eskaarAS <eskaaras@gmail.com>
2025-02-19 21:30:47 +0100

    Refactor translate pipe to support locale checks and improve translation logic

--- client/src/app/pipes/translate.pipe.ts
+++ client/src/app/pipes/translate.pipe.ts
@@ -1,14 +1,35 @@
-import { LOCALE_ID, Pipe, PipeTransform, inject } from '@angular/core';
-import { TranslationKey } from './text-mappings';
+import {
+  computed,
+  inject,
+  LOCALE_ID,
+  Pipe,
+  PipeTransform,
+} from '@angular/core';
+import {
+  textMappings,
+  TranslationKey,
+  TranslationLanguage,
+  TranslationLanguages,
+} from './text-mappings';
 
 @Pipe({
   name: 'translatePipe',
   pure: true,
 })
 export class TranslatePipe implements PipeTransform {
-  locale: string = inject(LOCALE_ID);
+  locale = inject(LOCALE_ID);
+  language = computed(() => this.locale.split('-')[0]);
+  isLocaleSupported = computed(() => {
+    console.log('this.language()', this.language());
+    return Object.values(TranslationLanguages).includes(
+      this.language() as TranslationLanguage
+    );
+  });
 
   transform(key: TranslationKey): string {
-    return key + this.locale;
+    const test = this.isLocaleSupported() as any;
+    console.log('test', test);
+    if (!this.isLocaleSupported()) return textMappings[key].en;
+    return textMappings[key][this.language() as TranslationLanguage];
   }
 }
