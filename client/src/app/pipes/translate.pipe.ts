e18e4c804009ee297772ae0f3627a9714d1f3c21
eskaarAS <eskaaras@gmail.com>
2025-02-19 22:16:15 +0100

    Refactor translate pipe to improve language support logic and simplify transform method

--- client/src/app/pipes/translate.pipe.ts
+++ client/src/app/pipes/translate.pipe.ts
@@ -18,23 +18,14 @@ import {
 })
 export class TranslatePipe implements PipeTransform {
   locale = inject(LOCALE_ID);
-  language = computed(() => this.locale.split('-')[0]);
-  isLocaleSupported = computed(() => {
-    return Object.values(TranslationLanguages).includes(
-      this.language() as TranslationLanguage
-    );
+  language = computed(() => this.locale.split('-')[0] as TranslationLanguage);
+  isLanguageSupported = computed(() => {
+    return Object.values(TranslationLanguages).includes(this.language());
   });
 
   transform(key: TranslationKey): string {
-    const test = 213123123;
+    const targetLanguage = this.isLanguageSupported() ? this.language() : 'en';
 
-    const maybe = (test * 10) as any;
-
-    // const test = this.isLocaleSupported() as any;
-    // console.log('test', test);
-    if (!this.isLocaleSupported()) return textMappings[key].en;
-    return (
-      textMappings[key][this.language() as TranslationLanguage] + '!' + maybe
-    );
+    return textMappings[key][targetLanguage];
   }
 }
